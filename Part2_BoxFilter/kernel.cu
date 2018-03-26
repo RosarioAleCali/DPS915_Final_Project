17f64e7412c6609a2f15c3b337c3ffa755ecbd7c
Max Fainshtein <mfainshtein4@myseneca.ca>
2018-03-26 19:42:19 -0400

    working part2

--- Part2_BoxFilter/kernel.cu
+++ Part2_BoxFilter/kernel.cu
@@ -1,76 +1,58 @@
-
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-#include "opencv2/highgui/highgui.hpp"
+#include "kernels.h"
 #include <iostream>
-#include <math.h>
-#include <chrono>
-#include <stdio.h>
+#include <cmath>
+#include <cuda_runtime.h>
 
-using namespace cv;
-const int ntpb = 32; // number of threads per block
-__global__ void NeighborKernel(Mat * src, Mat * result, int i, int j, int neighbour) {
-	int x = blockIdx.x * blockDim.x + threadIdx.x;
-	int y = blockIdx.y * blockDim.y + threadIdx.y;
-	int row_limit = src.rows; 
-	int column_limit = src.cols;
-	Scalar temp;
-	double sum = 0, blue = 0, red = 0, green = 0;
 
-	for (int i = x - floor(neighbour / 2); i <= x + floor(neighbour / 2); i++) {
-		for (int j = x - floor(neighbour / 2); j <= y + floor(neighbour / 2); j++) {
-			if (i >= 0 && j >= 0 && i < row_limit && j < column_limit) {
-				temp = src.at<Vec3b>(x, y);
-				blue += temp.val[0];
-				green += temp.val[1];
-				red += temp.val[2];
+__global__
+void blur(unsigned char* input_image, unsigned char* output_image, int width, int height, int neighbour) {
+
+	const unsigned int offset = blockIdx.x*blockDim.x + threadIdx.x;
+	int x = offset % width;
+	int y = (offset - x) / width;
+	if (offset < width*height) {
+
+		float output_red = 0;
+		float output_green = 0;
+		float output_blue = 0;
+		int hits = 0;
+		for (int ox = -neighbour; ox < neighbour + 1; ++ox) {
+			for (int oy = -neighbour; oy < neighbour + 1; ++oy) {
+				if ((x + ox) > -1 && (x + ox) < width && (y + oy) > -1 && (y + oy) < height) {
+					const int currentoffset = (offset + ox + oy*width) * 3;
+					output_red += input_image[currentoffset];
+					output_green += input_image[currentoffset + 1];
+					output_blue += input_image[currentoffset + 2];
+					hits++;
+				}
 			}
 		}
+		output_image[offset * 3] = output_red / hits;
+		output_image[offset * 3 + 1] = output_green / hits;
+		output_image[offset * 3 + 2] = output_blue / hits;
 	}
-	result.at<Vec3b>(x, y)[0] = blue / pow(neighbour, 2);
-	result.at<Vec3b>(x, y)[1] = green / pow(neighbour, 2);
-	result.at<Vec3b>(x, y)[2] = red / pow(neighbour, 2);
 }
-void findingNeighbors(Mat * src, Mat * result, int i, int j, int neighbour) {
-	Mat *Md, *Pd;
-	int nb = (i + ntpb - 1) / ntpb;
-	cudaMalloc((void**)&Md, i * j * sizeof(int));
-	cudaMemcpy(Md, src, i * j * sizeof(int), cudaMemcpyHostToDevice);
-	cudaMalloc((void**)&Pd, i * j * sizeof(int));
-	dim3 dGrid(nb, nb, 1);
-	dim3 dBlock(ntpb, ntpb, 1);
-	NeighborKernel<<<dGrid, dBlock >>>(src, result, i, j, neighbour); 
-	cudaMemcpy(result, Pd, i * j * sizeof(float), cudaMemcpyDeviceToHost);
-	cudaFree(Md);
-	cudaFree(Pd);
+
+
+void filter(unsigned char* input_image, unsigned char* output_image, int width, int height, int neighbour) {
+
+	unsigned char* dev_input;
+	unsigned char* dev_output;
+	cudaMalloc((void**)&dev_input, width*height * 3 * sizeof(unsigned char));
+	cudaMemcpy(dev_input, input_image, width*height * 3 * sizeof(unsigned char), cudaMemcpyHostToDevice);
+
+	cudaMalloc((void**)&dev_output, width*height * 3 * sizeof(unsigned char));
+
+	dim3 blockDims(512, 1, 1);
+	dim3 gridDims((unsigned int)ceil((double)(width*height * 3 / blockDims.x)), 1, 1);
+
+	blur << <gridDims, blockDims >> >(dev_input, dev_output, width, height, neighbour);
+
+
+	cudaMemcpy(output_image, dev_output, width*height * 3 * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+
+	cudaFree(dev_input);
+	cudaFree(dev_output);
+
 }
-void reportTime(const char* msg, std::chrono::steady_clock::duration span) {
-	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(span);
-	std::cout << msg << " - took - " <<
-		ms.count() << " millisecs" << std::endl;
-}
-int main(int argc, char* argv[]) {
-	std::string file = argv[1];
-	auto ts = std::chrono::steady_clock::now();
-	Mat src = imread(file, -1);
-	Mat result = imread(file, -1);
-	if (src.empty())
-		std::cout << "failed to open img.jpg" << std::endl;
-	else
-		std::cout << "img.jpg loaded OK" << std::endl;
-	Vec3b color;
-	float blue, green, red;
-	int row_limit = src.rows;
-	int column_limit = src.cols;
-	int called = 0;
-	std::cout << "x: " << row_limit << std::endl;
-	std::cout << "y: " << column_limit << std::endl;
-	findingNeighbors(&src, &result, row_limit, column_limit, atoi(argv[2]));
-	imshow("box filter", result);
-	imshow("original", src);
-	std::cout << "called:		" << called << std::endl;
-	auto te = std::chrono::steady_clock::now();
-	reportTime("Computation", te - ts);
-	waitKey(0);
-	destroyWindow("Lab 3");
-}
+
